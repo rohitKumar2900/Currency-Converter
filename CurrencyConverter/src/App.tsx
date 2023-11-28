94d702c043636cb9241f7bf480ef5d519b6cd18c
rohitKumar2900 <rohitvirgin69@gmail.com>
2023-11-28 14:40:22 +0530

    working response to the input box

--- CurrencyConverter/src/App.tsx
+++ CurrencyConverter/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState, useEffect, useReducer } from 'react';
+
+function currencyInfoReducer(state, action) {
+  switch (action.type) {
+    case 'SET_DATA':
+      return { ...state, data: action.payload };
+    default:
+      return state;
+  }
+}
+
+function useCurrencyInfo(currency:string) {
+  const [state, dispatch] = useReducer(currencyInfoReducer, { data: {} });
+
+  useEffect(() => {
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+      .then((res) => res.json())
+      .then((res) => dispatch({ type: 'SET_DATA', payload: res[currency] }));
+  }, [currency]);
+
+  return state.data;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedCurrency, setSelectedCurrency] = useState("inr");
+  const currencyInfo = useCurrencyInfo(selectedCurrency);
+
+  // Display loading or error state while data is being fetched
+  if (!currencyInfo) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>        
+        <button onClick={() => console.log(currencyInfo.inr)}>Click me</button>
+        <input
+          type="text"
+          value={selectedCurrency}
+          onChange={(e) => setSelectedCurrency(e.target.value) } 
+          placeholder="ex usd"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ CurrencyConverter/src/web/converter.tsx
@@ -0,0 +1,7 @@
+export default function Converter(){
+    return(
+        <div>
+            <h1>THis is converter area</h1>
+        </div>
+    )
+}
